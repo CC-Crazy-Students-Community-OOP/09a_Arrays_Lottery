7383b5ef554ad57d3d15751f003318f5855714dc
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-28 13:32:04 +0100

    kommentare und formatierung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,49 +8,42 @@ import java.util.Random;
 import java.util.Arrays;
 
 public class App {
-
 	public static void main(String[] args) {
-		
 		// Deklarationen
 		Random rand = new Random();
 		int min  = 1, max = 49, slots = 6;
-		int[] lotteryArray = new int[slots];
+		int[] lotteryArray = new int[ slots ];
 		int randomNumber;
 		boolean isRepeated;
 				
 		
-		for (int indexDrawn = 0; indexDrawn < slots; indexDrawn++) // 6x
-		{
+		// 6 mal die Schleife durchlaufen
+		for ( int indexDrawn = 0; indexDrawn < slots; indexDrawn++ ) {
 			do {
+					// setze isRepeated erst mal auf false, da bis jetzt die Zahl noch nie da gewesen sein kann
 					isRepeated = false;
-					// 1 to 49
+					// 1 to 49, eine random Zahl wird ermittelt und in randomNumber gespeichert
 					randomNumber = rand.nextInt((max - min) + 1) + min;
-					// check for repeated number against
-					// the filled non-zero elements			
-					for (int k = 0; k <= indexDrawn; k++) 
-					{
-						//check against each filled non-zero elements
-						//hope that none repeats the random number							
-						if (lotteryArray[k] == randomNumber) 
-						{
+					// checke in der Schleife ob die Nummer bereits in einem non-zero Element vorhanden ist
+					for ( int k = 0; k <= indexDrawn; k++ ) {
+						// checke in der Schleife in allen non-zero Element, ob die Nummer vorhanden ist						
+						if ( lotteryArray[k] == randomNumber ) {
+							// Wenn vorhanden, dann setzte isRepeated afu true und brich ab
 							isRepeated = true;
 							break;
 						}
 					}
 				
-				}while (isRepeated);
-			
-			lotteryArray[indexDrawn] = randomNumber;  // Zahl ins Array
-					
+				} while ( isRepeated );
+			// Zahl wird ins Array geschrieben, wenn eben noch nicht vorhanden
+			lotteryArray[ indexDrawn ] = randomNumber;
 		}
-		
 		Arrays.sort(lotteryArray);
+		// System.out.println(App.c( "#y" ) + "The result of the lottery:");	// println als eigene Zeile als output
+		System.out.print(App.c( "#y" ) + "The result of the lottery:");	// print als folgender output, immer ans LEerzeichen zwischen Ausgaben denken
 		
-		System.out.println("The result of the lottery:");
-		
-		for (int i = 0; i<slots; i++) 
-		{
-			System.out.print(lotteryArray[i] + " ");
+		for (int i = 0; i<slots; i++) {
+			System.out.print( "\t" + App.c( "#g" ) + lotteryArray[ i ] + " " );
 		}
 		
 	}
